289570a853307f8e4261a0a64bd2065217745b21
tienbot1 <tien.ru@mail.ru>
2023-05-11 23:05:15 +0900

    react-gh-pages

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,6 +4,7 @@ import s from './Card.module.scss';
 export const Card = ({symbol, title, keywords}) => {
   return (
     <div className={s.card}>
+        
         <p>{symbol}</p>
         <p>{title}</p>
         <p>{keywords}</p>
